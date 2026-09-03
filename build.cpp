476025331e659fc098a018361864176d01164ee3
Ariaszzzhc <ariaszzzhc@outlook.com>
2026-09-03 15:54:54 +0800

    feat: export tomlplusplus C++ module

--- build.cpp
+++ build.cpp
@@ -4,6 +4,8 @@ import std;
 int main() {
     bake::Builder b;
     const auto upstream = std::string(b.dep_src_dir("upstream"));
+    b.include_dirs(upstream + "/include");
     b.public_headers(upstream + "/include");
+    b.public_modules("module/*.cppm");
     return b.build();
 }
